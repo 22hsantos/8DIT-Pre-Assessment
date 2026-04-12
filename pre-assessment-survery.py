7ebbd2391ddf25d22e90b8838d06341ef05b34ed
22hsantos <22hsantos@wakatipu.school.nz>
2026-04-12 21:39:34 +1200

    Delete pre-assessment-survery.py
    Typo in the name. Will add fixed file name.

--- pre-assessment-survery.py
+++
@@ -1,9 +0,0 @@
-"""
--Make a GUI application which collects name, age and the answer to the survey question "Do you have a mobile phone?".
-
--When an Enter Data button is pressed, the information is collected from the GUI and used to construct an object for this person, stored in a list.
-
--When a Show All button is pressed, the data collection frame is removed and replaced by a frame in which each person's information is presented one by one using the Next/Previous buttons.
-
--An 'Add New Person' button will toggle the layout back to "data collection mode".
-"""
